aea7c3e70a83854c470c996c3f17404b82c1bbe6
felipecanetecanales <felipe.canetec@sansano.usm.cl>
2017-06-14 01:23:16 -0400

    Create tx_sequence.v

--- esclavo_endpoint_uart/esclavo_endpoint_uart.srcs/sources_1/new/tx_sequence.v
+++ esclavo_endpoint_uart/esclavo_endpoint_uart.srcs/sources_1/new/tx_sequence.v
@@ -18,9 +18,49 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
+module TX_sequence 
+(
+	input clock,
+	input reset,
+	input PB,             //PushButton
+	output reg send16,    // Si esta alto, se deben transmitir 16 bits (2 bytes)
+    input busy);         // Si esta alto, la UART se encuentra transmitiendo un dato
+    
+    reg[1:0] next_state, state; 
+
+    //state encoding
+    localparam IDLE 		 = 2'd0;  // Esperando dato
+    localparam TX_RESULTADO  = 2'd1;  // Transmitiendo el primer operando
+
+    // combo logic of FSM
+    always@(*) begin
+        //default assignments
+        next_state = state;
+        send16 = 1'b1;
+    	
+    	case (state)
+    		IDLE: 	begin
+						if(PB & ~busy) begin
+							next_state=TX_RESULTADO;
+						end
+					end
 
 
-module tx_sequence(
+            TX_RESULTADO: begin
+                         
+                            if(~busy) begin
+                                next_state=IDLE;
+                            end
+						end	
+    	endcase
+    end	
 
-    );
+    //when clock ticks, update the state
+    always@(posedge clock) begin
+    	if(reset)
+    		state <= IDLE;
+    	else
+    		state <= next_state;
+	end
+	
 endmodule
